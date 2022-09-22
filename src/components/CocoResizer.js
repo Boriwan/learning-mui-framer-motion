9248f47ef71dd8f1bd4ea30687d8689a0ea4fe95
Boriwan <boris.bocek@educanet.com>
2022-09-23 01:20:13 +0200

    animation done

--- src/components/CocoResizer.js
+++ src/components/CocoResizer.js
@@ -4,11 +4,27 @@ import Slider from '@mui/material/Slider';
 import { useState } from "react";
 
 export default function CocoResizer() {
-    const [x, setX] = useState(0);
+    const [x, setX] = useState(0)
+    const [y, setY] = useState(0)
+    const [scale, setScale] = useState(1)
+    const [rotate, setRotation] = useState(0)
 
     const changeX = (event, x) => {
         setX(x)
     }
+
+    const changeY = (event, y) => {
+        setY(y)
+    }
+
+    const changeScale = (event, scale) => {
+        setScale(scale)
+    }
+
+    const changeRotation = (event, rotate) => {
+        setRotation(rotate)
+    }
+
     return (
         <div>
             <h1>Scale</h1>
@@ -16,9 +32,9 @@ export default function CocoResizer() {
                 <motion.div
                     animate={{
                         x,
-                        y: 0,
-                        scale: 1,
-                        rotate: 0,
+                        y,
+                        scale,
+                        rotate,
                     }}
                     style={{
                         background: "aqua",
@@ -33,15 +49,48 @@ export default function CocoResizer() {
 
             <div className='slider'>
                 <p>x position</p>
-                <Slider defaultValue={x}  style={{
-                        width: "50%",
-                        marginLeft: 15,
-                        color: "aqua",
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                }} 
-                onChange={changeX} aria-label="Default" valueLabelDisplay="auto" min={-100} max={100} />
+                <Slider defaultValue={x} style={{
+                    width: "50%",
+                    marginLeft: 15,
+                    color: "aqua",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+                    onChange={changeX} aria-label="Default" valueLabelDisplay="auto" min={-100} max={100} />
+
+                <p>y position</p>
+                <Slider defaultValue={y} style={{
+                    width: "50%",
+                    marginLeft: 15,
+                    color: "aqua",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+                    onChange={changeY} aria-label="Default" valueLabelDisplay="auto" min={-100} max={100} />
+
+                <p>scale</p>
+                <Slider defaultValue={scale} style={{
+                    width: "50%",
+                    marginLeft: 15,
+                    color: "aqua",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+                    onChange={changeScale} aria-label="Default" valueLabelDisplay="auto" min={1} max={5} step={0.5} />
+
+                <p>rotate</p>
+                <Slider defaultValue={rotate} style={{
+                    width: "50%",
+                    marginLeft: 15,
+                    color: "aqua",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+                    onChange={changeRotation} aria-label="Default" valueLabelDisplay="auto" min={0} max={360} />
             </div>
 
         </div>
